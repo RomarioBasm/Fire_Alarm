1de7e88b89d4b6116b8ff80e9ebebd805b51a3c8
Dounia Khaled <82269866+douniakhaled@users.noreply.github.com>
2022-01-01 16:59:14 +0200

    adding controller

--- FireAlarmG3_JavaProject/src/firealarmg3_javaproject/FXMLDocumentController.java
+++ FireAlarmG3_JavaProject/src/firealarmg3_javaproject/FXMLDocumentController.java
@@ -4,12 +4,17 @@
  */
 package firealarmg3_javaproject;
 
+import eu.hansolo.medusa.Gauge;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -17,15 +22,55 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+  
+    @FXML
+    private Gauge thermometer;
+    @FXML
+    private Button help;
+    @FXML
+    private Button history;
     @FXML
     private Label label;
+    @FXML
+    private AnchorPane A2;
     
     @FXML
+    private AnchorPane A1;
+    @FXML
+    private Tab tab1;
+    
+    @FXML
+    private Tab tab2;
+    @FXML
+    private TabPane tabpane;
+
+    
+    
+   
+    
+
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+       
+        thermometer.setValue(60.00);
+        
+    }
+    @FXML
+    private void handleHelpAction(ActionEvent event) {
+       
+        thermometer.setValue(60.00);
+        
+    }
+    @FXML
+    private void handleHistoryAction(ActionEvent event) {
+       
+ 
+        this.tabpane.getSelectionModel().select(this.tab2);
+        thermometer.setValue(30.00);
+        
     }
     
+    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
