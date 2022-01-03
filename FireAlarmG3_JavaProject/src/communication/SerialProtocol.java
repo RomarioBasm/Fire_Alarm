9d31f68d9079f2d0f3fa96c6fd928aaf64803f75
hagermohmed256 <52625853+hagermohmed256@users.noreply.github.com>
2022-01-03 10:11:32 +0200

    method to (create a new connection between HW&SW)

--- FireAlarmG3_JavaProject/src/communication/SerialProtocol.java
+++ FireAlarmG3_JavaProject/src/communication/SerialProtocol.java
@@ -3,4 +3,31 @@ package communication;
 
 public class SerialProtocol {
     
+    Scanner in;              // an in stream of bytes. accepts input bytes.
+    OutputStream out;        // an output stream of bytes. accepts bytes and sends them to the hardware. 
+    static int temper;       // int variable that represent the 
+    static SerialPort port;  // serialPort, that represent the port.
+    
+    /********************** arduinoConnection  **********************
+    * This method create a new connection between the hardware and -*
+    * the software -------------------------------------------------*
+    * @param                                                        *
+    * @return                                                       *
+    ****************************************************************/
+    public void arduinoConnection() {
+        Vector<String> portList = new Vector<String>();
+        SerialPort[] portNames = SerialPort.getCommPorts();
+        String comPort = "COM3";
+//                for (int i = 0; i < portNames.length; i++) {
+//                    portList.add(portNames[i].getSystemPortName());
+//                    System.out.println(i + "- " + portNames[i].getSystemPortName());
+//                    comPort = portNames[i].getSystemPortName();
+//                }
+        port = SerialPort.getCommPort(comPort);
+        port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+        in = new Scanner(port.getInputStream());
+        port.openPort();
+    }
+    
+    
 }
