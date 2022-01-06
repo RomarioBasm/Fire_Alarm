a4c23fcd7f2811e6bd124e62bb5246acfa61c7e0
LAPTOP-8LUL3EEN\sara <saraadelelsayed33@gmail.com>
2022-01-06 22:04:13 +0200

    commenting linked list classes

--- FireAlarmG3_JavaProject/src/server/FireOccurenceList.java
+++ FireAlarmG3_JavaProject/src/server/FireOccurenceList.java
@@ -14,42 +14,47 @@ import java.io.*;
 
 public class FireOccurenceList {
 
-    private FireOccurenceNode head;
-    private String recordFileDirectory;
-    File file;
+    private FireOccurenceNode head; //creating a refrence to FireOccurenceNode class
+    private String recordFileDirectory; //a string refrence to save a file path 
+    File file; //creating a refrence to file class
 
+    // class constructor to intailize the start of the list by null at first
     FireOccurenceList(){
 	head=null;
     }
-
+    //method to add node at the end of the list and recives an object of class FireOccurenceNode
     public void appendNode(FireOccurenceNode data){
-        FireOccurenceNode node= data;
 
-        if (head==null){
+        FireOccurenceNode node= data;//creating a dummy node
+        //in case no nodes in the list assign the start of the list o the given node
+        if (head==null){ 
             head = node;
-        }else{
-        FireOccurenceNode fire=head;
+        }else{//loop till the end of the list is found
+        FireOccurenceNode fire=head; //creat a refrence to FireOccurenceNode and intalize it to the start node of the list
+        //check if it is the last node in the while condition
         while(fire.nextFireOccurence !=null){
-            fire=fire.nextFireOccurence;
+            fire=fire.nextFireOccurence;//assign the refrence to the next node
         }
-        fire.nextFireOccurence=node;
+        fire.nextFireOccurence=node;//add the new node after the last node
         }
-        FireOccurenceNode.TotalFireOccurences++;
+        FireOccurenceNode.TotalFireOccurences++; //after adding the node incrment the total fires by 1
     }
-
+    // method to search for a node by its fire order number and iit takes intger and retures an object of FireOccurenceNode class
     public FireOccurenceNode getNode(int id){
-        FireOccurenceNode node=head;
-
+        FireOccurenceNode node=head; //creat a refrence to FireOccurenceNode and intalize it to the start node of the list
+        //the condition terminates when the refrence reaches the last node 
         while(node.nextFireOccurence !=null){
+        //check if the node id is the same to refrence node id 
             if (node.getFireId()== id){
-                return node;
+                return node; //node is found and return it
             }
-            node=node.nextFireOccurence;
+            node=node.nextFireOccurence; //if condition is not true set refrence to the next node
         }
+        //check for the last node if its id matches with the refrence id 
         if (node.getFireId()== id){
-            return node;
+            return node; // node is found and return it
         }
-        return null;
+        return null; //if non of the conditions is true so the node is not found and the method returns null
     }
 
     public void deleteNode(int id){
@@ -78,64 +83,70 @@ public class FireOccurenceList {
       FireOccurenceNode.TotalFireOccurences--; 
 
     }
-
+    //method that takes a path of a file as a string 
     public void setRecordFileDirectory(String fileDirectory) throws FileNotFoundException{
-        recordFileDirectory= fileDirectory;
-        this.file=new File(recordFileDirectory);
+        recordFileDirectory= fileDirectory; //set the path to the given oath
+        this.file=new File(recordFileDirectory); // 
     }
 
     public String getHistoryContent(){
 
-        String recordfileContents="";        
+        String recordfileContents="";
+        String fireIdStr="";             
         FireOccurenceNode node=head;
         if(node==null){
             System.out.println("no fires occured");
+            recordfileContents= recordfileContents.concat("no fires occured");
         }
 
        while(node.nextFireOccurence !=null){
-            recordfileContents.concat("fireID:"+node.getFireId()+"\t fireDate:"+
+            recordfileContents=recordfileContents.concat("fireID:"+fireIdStr.valueOf(node.getFireId())+"\t fireDate:"+
             node.getFireOccurenceDate()+"\t fireTime:"+
             node.getFireOccurenceTime());
             node=node.nextFireOccurence;
         }
-        recordfileContents.concat("fireID:"+node.getFireId()+"\t fireDate:"+
+        recordfileContents=recordfileContents.concat("fireID:"+fireIdStr.valueOf(node.getFireId())+"\t fireDate:"+
         node.getFireOccurenceDate()+"\t fireTime:"+node.getFireOccurenceTime());
          return  recordfileContents;   
     }
 
+    //method to write the fires occured in the text file of the given path
     public void recordData() throws IOException{
+        //creat a filewriter reference pints to an objject and put the given file path in the constructor
     	FileWriter writer=new FileWriter(recordFileDirectory);
-    	String recordfileContents=""; 
-        String fireIdStr="";       
-        FireOccurenceNode node=head;
-        if(node==null){
+    	String recordfileContents=""; //creat an empty string to record in it the fire data
+        String fireIdStr="";       //creat an empty string to change fire id fro int to string
+        FireOccurenceNode node=head; //creat a refrence to fire occurence node and st it to the start of the list
+        if(node==null){ //if there is no nodes
             System.out.println("no fires occured");
-                recordfileContents= recordfileContents.concat("no fires occured");
-            writer.write(recordfileContents);
-           writer.close();
-            return;
+                recordfileContents= recordfileContents.concat("no fires occured");//set the empty string to no fires 
+            writer.write(recordfileContents);//write the string in the file
+            writer.close(); //end writing
+            return; //end the method
         }
-
+        //till last node is reached
        while(node.nextFireOccurence !=null){
            recordfileContents= recordfileContents.concat("fireID:"+fireIdStr.valueOf(node.getFireId())+"\t fireDate:"+
             node.getFireOccurenceDate()+"\t fireTime:"+
-            node.getFireOccurenceTime()+"\n");
-            node=node.nextFireOccurence;
+            node.getFireOccurenceTime()+"\n"); // add the current node's data to the string
+            node=node.nextFireOccurence; //point the refrence to the next node
 
         }
         System.out.println("entered first node");
         recordfileContents=recordfileContents.concat("fireID:"+fireIdStr.valueOf(node.getFireId())+"\t fireDate:"+
             node.getFireOccurenceDate()+"\t fireTime:"+
-            node.getFireOccurenceTime()+"\n");
-        writer.write(recordfileContents);
-        writer.close();
+            node.getFireOccurenceTime()+"\n"); //add the last node's data to the string
+        writer.write(recordfileContents); // write the stringin the file
+        writer.close(); // stop writing
 
     }
     
     
-
+    //method to display the history in a  new desktop window
     public void openHistory() throws IOException{
+        //creat a refrence to desktop class to acess the descktop of your computer
         Desktop recordHistoryTextFile=Desktop.getDesktop();
+        //open a window containg the text file in the given path 
         recordHistoryTextFile.open(file);
     }
 
--- FireAlarmG3_JavaProject/src/server/FireOccurenceNode.java
+++ FireAlarmG3_JavaProject/src/server/FireOccurenceNode.java
@@ -14,45 +14,58 @@ import java.util.Date;
 
 public class FireOccurenceNode {
 
-    private int fireId;
-    FireOccurenceNode nextFireOccurence;
-    private String fireOccurenceDate;
-    private String fireOccurenceTime;
-    static int TotalFireOccurences = 0;
+    private int fireId; // private intger member to the order  of fire since operation
+    FireOccurenceNode nextFireOccurence; //reference to FireOccurenceNode contain the address of the next node
+    private String fireOccurenceDate; // private string member to show fire date: day/month/year
+    private String fireOccurenceTime; //private string member to show fire time: hours:miniutes:seconds am/pm
+    static int TotalFireOccurences = 0; //declaring & intalizeing static intger member to show the total number of fires occured since operation
 
+    //constructor to be used while creating a dummy node  
     FireOccurenceNode(){
-
-	this.fireId = 1000;
+        //setting a large (far) id as it will be a dummy node
+	this.fireId = 1000; 
+        //creating a refrence to date class and store a new created object of class date in it
         Date currentDate = new Date();
+        //changing the date format to be day/month/year 
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/Y");
+        //changing the type from date to string and store it in fireOccurenceDate member
         this.fireOccurenceDate = dateFormat.format(currentDate);
+        //changing the date format to be hours:miniutes:seconds am/pmhours:miniutes:seconds am/pm
         SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        //changing the type from date to string and store it in fireOccurencetime member
         this.fireOccurenceTime = timeFormat.format(currentDate);
+        //initalize the next fire occurence refrence to null  
         this.nextFireOccurence=null;
     }
-
+    //a constructor to be used while creating a node when a fire occured and give the fire an order number (ID)
     FireOccurenceNode(int id){
-       this.fireId=id;
+        this.fireId=id; // set the fire order number of the fire
+        //creating a refrence to date class and store a new created object of class date in it
         Date currentDate=new Date();
+        //changing the date format to be day/month/year 
         SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/Y");
+        //changing the type from date to string and store it in fireOccurenceDate member
         this.fireOccurenceDate=dateFormat.format(currentDate);
+        //changing the date format to be hours:miniutes:seconds am/pmhours:miniutes:seconds am/pm
         SimpleDateFormat timeFormat=new SimpleDateFormat("hh:mm:ss a");
+        //changing the type from date to string and store it in fireOccurencetime member
         this.fireOccurenceTime=timeFormat.format(currentDate);
+        //initalize the next fire occurence refrence to null 
         this.nextFireOccurence=null;
     }
-
+    //setter for fire id that takes intger
     public void setFireId(int id){
         fireId =id;
     }
-
+    //getter for fire id that return intger
     public int getFireId(){
        return fireId;
     }
-
+    //getter for fireoccurence date that return string
     public String getFireOccurenceDate(){
        return fireOccurenceDate;
     }
-
+    //getter for fire occurence time that return string
     public String getFireOccurenceTime(){
        return fireOccurenceTime;
     }
